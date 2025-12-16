3f15e1b1271dce0394347094019a1f12b80ee739
Palak khare <palakkhare1902@gmail.com>
2025-12-16 16:23:21 +0530

    Modify database creation command for safety
    Updated database creation script to use 'IF NOT EXISTS' for safety.

--- 01 basics/create_database.sql
+++ 01 basics/create_database.sql
@@ -1,5 +1,10 @@
--- Create a new database for SQL practice
-CREATE DATABASE startersql;
+-- This command creates a database
+-- It will throw an error if the database already exists
+-- CREATE DATABASE startersql;
 
--- Use the database
+-- Safe way to create a database
+-- This will not throw an error if the database already exists
+CREATE DATABASE IF NOT EXISTS startersql;
+
+-- Select the database
 USE startersql;
